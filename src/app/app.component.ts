f62acbe45c433ba07f44bf9d9af7b9dd95d5daf4
mateus-bonette00 <mateus.bonette00@gmail.com>
2024-09-14 11:45:12 -0300

    Projeto adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<router-outlet></router-outlet>`,
+
 })
 export class AppComponent {
   title = 'app-to-do';
