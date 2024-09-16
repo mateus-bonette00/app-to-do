474c76f00b08447148b0fe84517662110122d045
mateus-bonette00 <mateus.bonette00@gmail.com>
2024-09-16 11:47:07 -0300

    configurações adicionados

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { TaskList } from '../../model/task-list'
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
+  public taskList: Array<TaskList> = [
 
+  ];
+
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked))
+  }
+
+  public setEmitTaskList(event: string){
+    this.taskList.push({task: event, checked: false});
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm('Você Deseja realmente deletar tudo?')
+
+    if(confirm){
+      this.taskList = [];
+    }
+  }
 }
